8b335726cbe7bef44a67c3593d1f89e2a9966b1c
KongRain <kongxygaoykk@163.com>
2015-07-02 21:44:38 +0800

    task0003
    获取数据部分完成

--- task0003/js/task0003.js
+++ task0003/js/task0003.js
@@ -7,11 +7,13 @@
     var tDetail = document.getElementById("task-detail");
     var classList = document.getElementsByClassName("class-list")[0];
     var data;
+    var current = {
+        class: "",
+        file: "",
+        task: ""
+    }; //当前查看的任务路径
 
-    /**
-     * 任务类型列表
-     */
-    function CreateLi (type) {
+    function createLi (type) {
         var type = type ? type : "folder";
         var li = document.createElement("li");
         var p = document.createElement("p");
@@ -24,10 +26,12 @@
         li.appendChild(p);
         return li;
     }
-    CreateLi.prototype = {
-        constructor: CreateLi,
 
-    }
+    /**
+     * 初始化任务类型列表
+     *
+     * 点击其中一个任务类型，出现其下属子文件
+     */
 
     function initTC(xhr, text) {
         var text = JSON.parse(text);
@@ -37,83 +41,43 @@
             var name = text[i].class;
             var files = text[i].file;
             var len = files.length;
-            var li = new CreateLi("folder");
+            var li = createLi("folder");
             var span = li.getElementsByTagName("span")[0];
             span.innerHTML = name + "(" + len + ")";
             list.appendChild(li);
             addEvent(li, "click", clickHandler);
         }
+
+
+        function clickHandler() {
+            var subList = this.getElementsByTagName("ul")[0];
+
+            if(subList) {
+                this.removeChild(subList);
+            }else {
+                var className = this.getElementsByTagName("span")[0].innerHTML;
+                className = className.replace(/\s*\(\d+\)$/, "");
+                current.class = className;
+                var options = {
+                    type: "GET",
+                    data: {
+                        "class": className
+                    },
+                    onsuccess: getData,
+                    onfail: failHandler
+                };
+                ajax.call(this, "file_data.json", options);
+            }
+        }
     }
 
-    function failHandler(xhr) {
-        console.log(xhr.error);
-    }
 
     /**
-     * 鼠标进入 退出
+     * 初始化子文件列表
+     * 点击其中一个子文件，在任务列表内显示出该文件下的任务
+     *
+     * @param obj 通过Ajax获得的当前任务类型下的子文件对象
      */
-    function onMouseEnter(e) {
-        var selector = "li";
-        var event = e || window.event,
-            target = event.target || event.srcElement,
-            related = event.relatedTarget || event.fromElement,
-            match;
-        while(target && target != document && !(match = matches(target, selector))) {
-            target = target.parentNode;
-        }
-        if(!match) {return;}
-        while(related && related != document && related != target) {
-            related = related.parentNode;
-        }
-        //消除内部干扰
-        if(related == target) {return;}
-
-        target.style.backgroundColor = "#B5B5B5";
-    }
-
-    function onMouseLeave(e) {
-        var selector = "li";
-        var event = e || window.event,
-            target = event.target || event.srcElement,
-            related = event.relatedTarget || event.toElement,
-            match;
-
-        while(target && target != document && !(match = matches(target, selector))) {
-            target = target.parentNode;
-        }
-        if(!match) {return;}
-        while(related && related != document && related != target) {
-            related = related.parentNode;
-        }
-        if(related == target) {return;}
-
-        target.style.background = "transparent";
-    }
-
-    function matches(ele, selector) {
-        return ele.webkitMatchesSelector(selector);
-    }
-
-    function clickHandler() {
-        var subList = this.getElementsByTagName("ul")[0];
-
-        if(subList) {
-            this.removeChild(subList);
-        }else {
-            var className = this.getElementsByTagName("span")[0].innerHTML;
-            className = className.replace(/\s*\(\d+\)$/, "");
-            var options = {
-                type: "GET",
-                data: {
-                    "class": className
-                },
-                onsuccess: getData,
-                onfail: failHandler
-            };
-            ajax.call(this, "file_data.json", options);
-        }
-    }
-
 
     function initSubTC(obj) {
         if(obj) {
@@ -123,44 +87,46 @@
             for(var j=0; j<len; j++) {
                 var fileName = obj[j].fname;
                 var tasks = obj[j].task;
-                var li = CreateLi("file");
+                var li = createLi("file");
                 var span = li.getElementsByTagName("span")[0];
                 span.innerHTML = fileName + "(" + tasks.length + ")";
                 ul.appendChild(li);
                 addEvent(li, "click", subClickHandler);
             }
         }
+
+        function subClickHandler(e) {
+            var event = e || window.event;
+            if(event.cancelBubble) {
+                event.cancelBubble = true;
+            } else {
+                event.stopPropagation();
+            }
+
+            var fileName = this.getElementsByTagName("span")[0].innerHTML;
+            fileName = fileName.replace(/\s*\(\d+\)$/, "");
+            current.file = fileName;
+
+            var options = {
+                type: "GET",
+                data: {
+                    "class": current.class,
+                    "fname": current.file
+                },
+                onsuccess: getData,
+                onfail: failHandler
+            }
+            ajax.call(this, "file_data.json", options);
+        }
     }
 
-    function subClickHandler(e) {
-        var event = e || window.event;
-        if(event.cancelBubble) {
-            event.cancelBubble = true;
-        } else {
-            event.stopPropagation();
-        }
-
-        var parent = this.parentNode;
-        while(parent.nodeName.toLowerCase() != "li") {
-            parent =  parent.parentNode;
-        }
-        var className = parent.getElementsByTagName("span")[0].innerHTML;
-        className = className.replace(/\s*\(\d+\)$/, "");
-        var fileName = this.getElementsByTagName("span")[0].innerHTML;
-        fileName = fileName.replace(/\s*\(\d+\)$/, "");
-        var options = {
-            type: "GET",
-            data: {
-                "class": className,
-                "fname": fileName
-            },
-            onsuccess: getData,
-            onfail: failHandler
-        }
-        ajax.call(this, "file_data.json", options);
-    }
-
-    //初始化任务列表 包括所有、完成、未完成
+    /**
+     * 初始化任务列表 包括所有、完成、未完成
+     * 点击其中一个任务，在任务细节框内显示此任务的内容、创建时间等
+     *
+     *
+     * @param obj 通过Ajax获得的当前子文件下任务对象
+     */
     function initTask(obj) {
        if(obj) {
            var len = obj.length;
@@ -248,6 +214,7 @@
                 addClass(taskBlock, "done");
             }
             dateItem.appendChild(taskBlock);
+            addEvent(taskBlock, "click", taskClickHandler);
         }
 
         //初始化未完成列表
@@ -262,6 +229,8 @@
                     if(parent.children.length == 1) {
                         unDoneList.removeChild(parent);
                     }
+                } else {
+                    addEvent(that, "click", taskClickHandler);
                 }
             }
         }
@@ -280,15 +249,52 @@
                     }
                 } else {
                     removeClass(that, "done");
+                    addEvent(that, "click", taskClickHandler);
                 }
             }
         }
 
+        function taskClickHandler() {
+            var name = this.innerHTML;
+            var s_name = tDetail.getElementsByClassName("task-name")[0];
+            s_name.innerHTML = name;
+            var body = tDetail.getElementsByClassName("detail")[0];
+            body.style.display = "block";
+            current.task = name;
+            var options = {
+                type: "GET",
+                data: {
+                    "class": current.class,
+                    "fname": current.file,
+                    "tname": current.task
+                },
+                onsuccess: getData,
+                onfail: failHandler
+            }
+            ajax.call(this, "file_data.json", options);
+        }
     }
 
+    /**
+     * 初始化任务细节框
+     *
+     * @param obj 通过Ajax获得的当前任务对象
+     */
+
+    function initDetail(obj) {
+        var s_date = tDetail.getElementsByClassName("date")[0];
+        s_date.innerHTML = "任务时间：" + obj.date;
+        var s_detail = tDetail.getElementsByClassName("detail-body")[0].getElementsByTagName("p")[0];
+        s_detail.innerHTML = obj.detail;
+    }
+
+    /**
+     * Ajax "GET"方式 处理函数，获得整体的json对象
+     * 根据查询字符串的不同再分别调用各自的处理函数
+     */
     function getData(xhr, text) {
         var file, task, detail, data, length;
-        var str = {};
+        var str = {};     //查询字符串
         var text = JSON.parse(text);
         length = text.length;
         var url = xhr.responseURL;
@@ -329,15 +335,73 @@
                     }
                 }
             } else if (!detail) {
-
+                var length3 = task.length;
+                for(var i = 0; i < length3; i++) {
+                    var that = task[i];
+                    if(that[k] == str[k]) {
+                        detail = that;
+                        break;
+                    }
+                }
             }
         }
         if (file && !task) {
             initSubTC.call(this, file);
         } else if (file && task && !detail) {
             initTask.call(this, task);
+        } else if(file && task && detail) {
+            initDetail.call(this, detail);
         }
     }
+
+    function failHandler(xhr) {
+        console.log(xhr.error);
+    }
+
+    /**
+     * 鼠标进入 退出
+     */
+    function onMouseEnter(e) {
+        var selector = "li";
+        var event = e || window.event,
+            target = event.target || event.srcElement,
+            related = event.relatedTarget || event.fromElement,
+            match;
+        while(target && target != document && !(match = matches(target, selector))) {
+            target = target.parentNode;
+        }
+        if(!match) {return;}
+        while(related && related != document && related != target) {
+            related = related.parentNode;
+        }
+        //消除内部干扰
+        if(related == target) {return;}
+
+        target.style.backgroundColor = "#B5B5B5";
+    }
+
+    function onMouseLeave(e) {
+        var selector = "li";
+        var event = e || window.event,
+            target = event.target || event.srcElement,
+            related = event.relatedTarget || event.toElement,
+            match;
+
+        while(target && target != document && !(match = matches(target, selector))) {
+            target = target.parentNode;
+        }
+        if(!match) {return;}
+        while(related && related != document && related != target) {
+            related = related.parentNode;
+        }
+        if(related == target) {return;}
+
+        target.style.background = "transparent";
+    }
+    function matches(ele, selector) {
+        return ele.webkitMatchesSelector(selector);
+    }
+
 
 
     window.onload = function() {
@@ -354,7 +418,6 @@
         addEvent(classList, "mouseout", function(e) {
             onMouseLeave(e);
         });
-
     }
 
 })();
